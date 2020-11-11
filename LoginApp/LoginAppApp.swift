137f6252fe07e0d1fef20cd0abb5a73fd542c353
tpk73 <tpaaka73@gmail.com>
2020-11-11 17:00:04 -0600

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Truett Knox on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
